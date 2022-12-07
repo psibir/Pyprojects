b85881f6c9c3f40b9a2b944aaa0d20c3995cf7a9
psibir <bloomfieldtm@gmail.com>
2022-12-06 21:29:11 -0600

    Update extensionrenamer.py

--- extensionrenamer.py
+++ extensionrenamer.py
@@ -1,16 +1,22 @@
 
 import os
 
-folder = input("Enter the absolute path of the directory containing files you wish to be changed: ")
-old_ext = input("Enter the previous file extension: ").removeprefix(".")
-new_ext = input("Enter the new file extension:  ").removeprefix(".")
 
-for filename in os.listdir(folder):
-    oldfilename = os.path.join(folder, filename)
-    if not os.path.isfile(oldfilename): continue
-    oldbase = os.path.splitext(filename)
-    if not old_ext == oldbase[1].removeprefix("."): continue
-    newname = (oldbase[0]+"."+new_ext)   
-    newfilename = os.path.join(folder,newname)
+def main():
+    folder = input("Enter the absolute path of the directory containing files you wish to be changed: ")
+    old_ext = input("Enter the previous file extension: ").removeprefix(".")
+    new_ext = input("Enter the new file extension:  ").removeprefix(".")
 
-    output = os.rename(oldfilename, newfilename)
+    for filename in os.listdir(folder):
+        oldfilename = os.path.join(folder, filename)
+        if not os.path.isfile(oldfilename): continue
+        oldbase = os.path.splitext(filename)
+        if not old_ext == oldbase[1].removeprefix("."): continue
+        newname = (oldbase[0]+"."+new_ext)   
+        newfilename = os.path.join(folder,newname)
+
+        output = os.rename(oldfilename, newfilename)
+        
+        
+if __name__ == '__main__':
+	main()
